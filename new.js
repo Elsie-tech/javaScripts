08bc858d3b65d472748da49b7a375aa7906366f7
Elsie <108669000+Elsie-tech@users.noreply.github.com>
2024-02-01 11:08:09 +0200

    first commit

---
+++ new.js
@@ -0,0 +1,44 @@
+// you can use it to write anything to your browsers.
+
+console.log('hi');
+console.log("I am Elsie");
+
+//variables: are placeholders to store information and data in your app or something else you are trying to develop.
+
+name = "Kayitankore Elsie";
+sentence = "I am learning javascript";
+
+//it can not show or see it into your browsers.
+
+console.log(name);
+console.log(sentence);
+
+//use console.log and recall your variable so it will enable you to see it .
+
+// operators: are used to assign values,compare values,perform arithmetic operations. stuff you see in maths class.
+
+a = 5;
+b = 10;
+sum = a+b;
+mult =a*b;
+div = a/b;
+mod = a%b;
+
+console.log (sum, mult , div, mod);
+food = 20000;
+tipPercentage = 0.2;
+tipAmount = food * tipPercentage;
+console.log(tipAmount);
+
+//user input by using The prompt(): is a built- in javascript functionality that helps you get inputs from a user through the browser.
+
+fruit = prompt(" what is your favority fruit");
+console.log (fruit);
+ 
+// alert() is like a prompt(),but it is only for the output
+ 
+ food = prompt("how much was the food?");
+ tipPercentage = prompt('tip %?') /100;
+ tipAmount = food * tipPercentage;
+ console.log (tipAmount);
+ alert (tipAmount);
